065242c4fa85873d9dcaf61d939d2450d70a6756
Antonio Iacobucci <lonelydevil1990@gmail.com>
2017-10-26 21:05:11 +0200

    Update drawer.java
    algorithm completed

--- drawer.java
+++ drawer.java
@@ -18,49 +18,68 @@ move(int b):
 */
 
 public void draw_horizontal(int n, int m){
-  for(int i=1; i<m+1; i++){
-    if((i % 2)!=0){                                 // i ODD: horizontal line towards right
-      for(int j=1; j<n+1; j++) draw(0);
-    }
-    else{                                           // i EVEN: horizontal line towards left
-      for(int j=1; j<n+1; j++) draw(1);
-    }
-  draw(3);
-  }  
+    for(int i=1; i<m+1; i++){
+        if((i % 2)!=0){                                     // i ODD: horizontal line towards right
+            for(int j=1; j<n; j++) draw(0);
+        }
+        else{                                               // i EVEN: horizontal line towards left
+            for(int j=1; j<n; j++) draw(1);
+        }
+        draw(3);
+    }  
 }
 
 public void draw_vertical(int n, int m){
-  bool controller= false;                           // m ODD: drawing from left to right
-  if((m % 2)==0) controller= true;                  // m EVEN: drawing from right to left
-  // first vertical line
-  int first= m;
-  while(first > 1){                                 // first vertical line has alternated drawn and blank borders starting with a blank one
-    draw(2);
-    move(2);
-    first= first-2;
-  }
-  if(first==1) draw(2);                             // drawing last border of the line if m ODD
-  // central vertical lines, no drawn borders
-  for(int j=2; j<n; i++){
-    if((j % 2)!=0){                                 // j ODD: vertical line towards up
-      for(int i=1; i<m+1; i++) draw(2);
+    bool controller= false;                                 // m ODD: drawing from left to right
+    if((m % 2)==0) controller= true;                        // m EVEN: drawing from right to left
+    // first vertical line has alternated drawn and blank borders starting with a blank one
+    int first= m;
+    while(first > 1){
+        draw(2);
+        move(2);
+        first= first-2;
     }
-    else{                                           // j EVEN: vertical line towards down
-      for(int i=1; i<m+1; i++) draw(3);
+    if(first==1) draw(2);                                   // drawing last border of the line if m ODD
+    if(controller) move(1);                                 // moving left or right
+    else move(0);
+    // central vertical lines have no drawn borders except horizontal ones
+    for(int j=2; j<n; j++){
+        if((j % 2)!=0){                                     // j ODD: vertical line towards up
+            for(int i=1; i<m; i++) draw(2);
+        }
+        else{                                               // j EVEN: vertical line towards down
+            for(int i=1; i<m; i++) draw(3);
+        }
+        if(controller) move(1);                             // MOVING left or right(horizontal lines are already drawn)
+        else move(0);
     }
-  if(controller) move(1);                           // MOVING left or right(horizontal lines are already drawn)
-  else move(0);
-  }
-  // last vertical line
-  for(int j=1; 1<m+1; m++){                         // last vertical line has alternated drawn and blank lines
-    if(controller && (((n+1) % 2)!=0)){             // n EVEN, m ODD: starting with a blank? border
+    // last vertical line has alternated drawn and blank borders
+    int last=m;
+    if(controller && ((n % 2)!=0)){                         // m EVEN, n ODD: line towards down starting with a blank border
+        while(last > 1){
+            draw(3);
+            move(3);
+            last= last-2;
+        }
+        if(last==1) draw(3);
     }
-    else{                                           // starting with a drawn? border
+    else if((n % 2)!=0)){                                   // m ODD, n ODD: line towards down starting with a drawn border
+        while(last > 1){
+            move(3);
+            draw(3);
+            last= last-2;
+        }
     }
-  }
+    else{                                                   // n EVEN: line towards up
+        while(last > 1){
+            move(2);
+            draw(2);
+            last= last-2;
+        }
+    }
 }
 
 public void solver(int n, int m){
-  draw_horizontal(n,m);                             // draws all the horizontal lines
-  draw_vertical(n,m);                               // draws all the vertical lines without drawing those already done 
+  draw_horizontal(n,m);                                     // draws all the horizontal lines
+  draw_vertical(n,m);                                       // draws all the vertical lines without drawing those already done 
 }
